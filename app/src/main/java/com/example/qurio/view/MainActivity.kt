b86c9802a060d4278a33c05861ba0c445dee0651
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2025-10-18 14:37:28 +0300

    fix: status bar padding

--- app/src/main/java/com/example/qurio/view/MainActivity.kt
+++ app/src/main/java/com/example/qurio/view/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.qurio.view
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.example.qurio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            v.updatePadding(top = statusBarInsets.top)
+            insets
+        }
     }
 }
